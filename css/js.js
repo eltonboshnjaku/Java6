8f4e8b228750deb7ec1a74b8b2d9771c1d897714
eltonboshnjaku <eb44025@ubt-uni.net>
2020-05-07 14:02:44 +0200

    ushtrimi commit

---
+++ css/js.js
@@ -0,0 +1,10 @@
+function postoLajme(){
+    var titulli=document.createElement('h3');
+    var p = document.createElement('p');
+    var titleText=document.createTextNode('titulliiiiiiii');
+    titulli.appendChild(titleText);
+    var bodytext=document.createTextNode('body body body');
+    document.createElement('p').appendChild(bodytext);
+    
+}
+
